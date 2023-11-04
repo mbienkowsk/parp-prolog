c7701bcef54a7a7cd763b0bdeff23dd650097f2f
maksym <01178511@pw.edu.pl>
2023-11-04 23:12:28 +0100

    added a game start setup file

---
+++ setup.pl
@@ -0,0 +1,11 @@
+
+/* These predicates get imported during the start of the game, set up whatever you want
+to test what you've implemented. */
+
+i_am_at(locker_room).
+
+path(locker, n, locker_room).
+
+at(locker, locker_room).
+is_closed(locker).
+
