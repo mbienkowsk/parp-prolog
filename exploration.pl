ab5eba2a08bea96ba1a33ea0906d27d997bef376
maksym <01178511@pw.edu.pl>
2023-11-04 23:17:58 +0100

    moved rules for examining thing and stopped using the notice_objects loop

--- exploration.pl
+++ exploration.pl
@@ -1,19 +1,24 @@
+
 /* This rule tells how to look around you. */
 
 look :-
     i_am_at(Place),
     describe(Place),
     nl,
-    notice_objects_at(Place),
+    % notice_objects_at(Place),
     nl.
+
+/* Rules responsible for examining items */
+examine(Thing) :- describe(Thing).
+inspect(Thing) :- examine(Thing).
 
 
 /* These rules set up a loop to mention all the objects
 in your vicinity. */
 
-notice_objects_at(Place) :-
-    at(X, Place),
-    write('There is a '), write(X), write(' here.'), nl,
-    fail.
+% notice_objects_at(Place) :-
+%     at(X, Place),
+%     write('There is a '), write(X), write(' here.'), nl,
+%     fail.
 
-notice_objects_at(_).
+% notice_objects_at(_).
