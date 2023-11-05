f4b8252148c2e8f94edf7f6c01e9538a0a4ff9fd
maksym <01178511@pw.edu.pl>
2023-11-05 20:21:54 +0100

    refactoring of go to use door mechanics

--- movement.pl
+++ movement.pl
@@ -13,12 +13,42 @@ w :- go(w).
 
 
 /* This rule tells how to move in a given direction. */
+
+
+% No path in the given direction
+go(Direction) :-
+    i_am_at(Here),
+    \+ door(Here, Direction, _),
+    !,
+    write('Cannot go from '), write(Here), write(' in this direction.').
+
+% door locked
+go(Direction) :-
+    i_am_at(Here),
+    is_locked(door(Here, Direction, _)),
+    !,
+    write('The door in the direction '), write(Direction), write(' is locked.').
+
+% door closed/unlocked
+go(Direction) :-
+    i_am_at(Here),
+    (is_closed(door(Here, Direction, _)); is_unlocked(door(Here, Direction, _))),
+    !,
+    write('You need to open the door first').
+
+% check explicitly if door is open
 go(Direction) :-
     i_am_at(Here),
     path(Here, Direction, There),
+    !,
     retract(i_am_at(Here)),
     assert(i_am_at(There)),
-    !, look.
+    look.
 
+% any other case (there shouldn't be any) - deny access
 go(_) :-
-    write('You can''t go that way.').
+    write('You can''t go that way.'),
+    !.
+
+% shorthand for checking whether one can go to a place
+path(Here, Direction, There) :- is_unlocked(door(Here, Direction, There)).
