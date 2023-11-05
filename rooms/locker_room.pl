b084bf015258b3a487da84f6b29322705217d0f5
maksym <01178511@pw.edu.pl>
2023-11-05 20:21:12 +0100

    locker room east door mechanics

--- rooms/locker_room.pl
+++ rooms/locker_room.pl
@@ -1,4 +1,5 @@
 :- discontiguous in/2.
+:- discontiguous is_closed/1.
 
 /* The locker */
 in(locker, locker_room).
@@ -8,5 +9,14 @@ is_closed(locker).
 /* The coat */
 in(coat, locker).
 
-/* The key */
+/* Key to the eastern door*/
 in(locker_room_key, coat).
+
+/* East door towards corridor 1*/
+door(locker_room, e, corridor_1).
+door_interface(east_door).
+east_door :- door(locker_room, e, corridor_1).
+is_locked(east_door).
+can_unlock(east_door) :- holding(locker_room_key).
+can_lock(east_door) :- holding(locker_room_key).
+
