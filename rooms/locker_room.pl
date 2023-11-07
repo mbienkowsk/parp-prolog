0d80f79b79e60c9f2dc43cc55a3eabff0eefc086
maksym <01178511@pw.edu.pl>
2023-11-07 21:15:53 +0100

    can_pick_up predicate to prevent user from picking up rooms, implemenation of security room combination lock

--- rooms/locker_room.pl
+++ rooms/locker_room.pl
@@ -1,5 +1,4 @@
-:- discontiguous in/2.
-:- discontiguous is_closed/1.
+:- discontiguous in/2, is_closed/1, can_pick_up/1, door/3, is_locked/1, door_interface/1.
 
 /* The locker */
 in(locker, locker_room).
@@ -8,9 +7,11 @@ is_closed(locker).
 
 /* The coat */
 in(coat, locker).
+can_pick_up(coat).
 
 /* Key to the eastern door*/
 in(locker_room_key, coat).
+can_pick_up(key).
 
 /* East door towards corridor 1*/
 door(locker_room, e, corridor_1).
@@ -20,3 +21,21 @@ is_locked(east_door).
 can_unlock(east_door) :- holding(locker_room_key).
 can_lock(east_door) :- holding(locker_room_key).
 
+/* Northern door towards security room*/
+door(locker_room, n, security_room).
+north_door :- door(locker_room, n, security_room).
+in(north_door, locker_room).
+door_interface(north_door).
+is_locked(north_door).
+
+/* Combination lock */
+in(keypad, locker_room).
+enter(keypad, 123456) :-
+    write('The light on the lock blinks green, it buzzes and unlocks. Correct!'),
+    !,
+    assert(is_open(north_door)).
+    % todo: make this not reset after leaving room!
+
+enter(keypad, _) :-
+    write('The light on the lock blinks red. The code provided was incorrect.'),
+    !.
