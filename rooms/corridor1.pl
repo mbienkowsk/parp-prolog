8fb89e9c71c9a086fc4af461a02a3420914e17dd
maksym <01178511@pw.edu.pl>
2023-11-07 21:56:47 +0100

    corridor 1 door descriptions

---
+++ rooms/corridor1.pl
@@ -0,0 +1,17 @@
+
+
+% locker room door to the west
+door(corridor_1, w, locker_room).
+west_door :- door(corridor_1, w, locker_room).
+door_interface(west_door).
+is_open(west_door).
+
+% corridor 2 path to the south
+door(corridor_1, s, corridor_2).
+is_open(door(corridor_1, s, corridor_2)).
+
+% generator room door to the north
+door(corridor_1, n, generator_room).
+north_door :- door(corridor_1, n, generator_room).
+door_interface(north_door).
+
