060fd79ad6979f2bca7de4791b62d09187583ae1
maksym <01178511@pw.edu.pl>
2023-11-11 15:27:25 +0100

    simplified door declarations

--- rooms/locker_room/logic.pl
+++ rooms/locker_room/logic.pl
@@ -1,4 +1,6 @@
-:- discontiguous in/2, is_closed/1, can_pick_up/1, door/3, is_locked/1, door_interface/1.
+
+:- discontiguous in/2, can_pick_up/1, door/3, is_locked/1.
+:- dynamic is_closed/1, is_locked/1, is_open/1, is_unlocked/1.
 
 /* The locker */
 in(locker, locker_room).
@@ -15,27 +17,21 @@ can_pick_up(key).
 
 /* East door towards corridor 1*/
 door(locker_room, e, corridor_1).
-east_door :- door(locker_room, e, corridor_1).
-door_interface(east_door).
-is_locked(east_door).
-in(east_door, locker_room).
-can_unlock(east_door) :- holding(locker_room_key).
-can_lock(east_door) :- holding(locker_room_key).
+is_locked(door(locker_room, e, corridor_1)).
+can_unlock(door(locker_room, e, corridor_1)) :- holding(locker_room_key).
+can_lock(door(locker_room, e, corridor_1)) :- holding(locker_room_key).
 
 /* Northern door towards security room*/
 door(locker_room, n, security_room).
-north_door :- door(locker_room, n, security_room).
-in(north_door, locker_room).
-door_interface(north_door).
-is_locked(north_door).
+is_locked(door(locker_room, n, security_room)).
 
 /* Combination lock */
 in(keypad, locker_room).
 enter(keypad, 123456) :-
     write('The light on the lock blinks green, it buzzes and unlocks. Correct!'),
     !,
-    assert(is_open(north_door)).
-    % todo: make this not reset after leaving room!
+    % todo - have to retract current state
+    door(locker_room, n, security_room).
 
 enter(keypad, _) :-
     write('The light on the lock blinks red. The code provided was incorrect.'),
