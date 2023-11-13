93f72cbbb7cb8e880a00934fa4f5deb1be562378
maksym <01178511@pw.edu.pl>
2023-11-13 16:46:08 +0100

    joined all logic.pl and descriptions.pl files to  avoid problems regarding overriding predicates

--- rooms/logic.pl
+++ rooms/logic.pl
@@ -1,5 +1,5 @@
 
-:- discontiguous in/2, can_pick_up/1, door/3, is_locked/1.
+:- discontiguous in/2, can_pick_up/1, door/3, is_locked/1, is_open/1.
 :- dynamic is_closed/1, is_locked/1, is_open/1, is_unlocked/1.
 
 /* The locker */
@@ -36,3 +36,21 @@ enter(keypad, 123456) :-
 enter(keypad, _) :-
     write('The light on the lock blinks red. The code provided was incorrect.'),
     !.
+
+/*-------------------------------------------
+END OF LOCKER_ROOM
+START OF CORRIDOR_1
+---------------------------------------------*/
+
+% locker room door to the west
+door(corridor_1, w, locker_room).
+is_open(door(corridor_1, w, locker_room)).
+
+% corridor 2 path to the south
+% todo
+% path(corridor_1, s, corridor_2).
+
+% generator room door to the north
+door(corridor_1, n, generator_room).
+is_open(door(corridor_1, n, generator_room)).
+
