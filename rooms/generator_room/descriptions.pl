548b01f93ca9fba785abe4d6b61edf7deda8671b
mgarbowski <mikolaj.garbowski@gmail.com>
2023-11-16 20:01:50 +0100

    fix missing comma

--- rooms/generator_room/descriptions.pl
+++ rooms/generator_room/descriptions.pl
@@ -2,7 +2,7 @@
 
 describe(generator_room) :-
     write('As you explore the generator room, the dormant generator at the center stands in'), nl,
-    write('stark contrast to the adjacent ventilation shaft. The idle turbines offer a'), nl
+    write('stark contrast to the adjacent ventilation shaft. The idle turbines offer a'), nl,
     write('glimpse into the halted functionality of the lab\'s power source. The ventilation'), nl,
     write('shaft, though vibrating with a faint breeze, seems large enough for someone to'), nl,
     write('fit through.'),
